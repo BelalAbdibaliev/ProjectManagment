0620a7bdf981d940d6c01998eed57ddf57056cd1
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-08 19:56:43 +0600

    Update entity

--- ProjectManagment.Domain/Entities/Project.cs
+++ ProjectManagment.Domain/Entities/Project.cs
@@ -5,12 +5,11 @@ public class Project
     public int Id { get; set; }
     public string Name { get; set; }
     
-    public Employee Manager { get; set; }
-    public int ManagerId { get; set; }
-    
     public ClientCompany Client {get; set;}
     public int ClientId { get; set; }
     
     public SupplierCompany Supplier {get; set;}
     public int SupplierId { get; set; }
+    
+    public ICollection<Employee> Employees { get; set; } = new List<Employee>();
 }
