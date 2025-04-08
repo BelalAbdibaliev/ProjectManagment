45dcca4e4262d9a1df98a71c277aaa5788fc2552
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-08 19:40:00 +0600

    Add DbContext and DbContextFactory

---
+++ ProjectManagment.Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectManagment.Domain.Entities;
+
+namespace ProjectManagment.Persistence.Context;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+        
+    }
+    
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<SupplierCompany> SupplierCompanies { get; set; }
+    public DbSet<ClientCompany> ClientCompanies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ ProjectManagment.Persistence/Context/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ProjectManagment.Persistence.Context;
+
+public class ApplicationDbContextFactory: IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+        optionsBuilder.UseSqlServer(
+            "Data Source=AcerAspire3;Initial Catalog=ProjectManagment-db;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        
+        return new ApplicationDbContext(optionsBuilder.Options);
+    }
+}
