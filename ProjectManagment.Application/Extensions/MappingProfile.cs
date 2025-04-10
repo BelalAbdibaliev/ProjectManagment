c75fceddb3aa142945091c1d6bf8012cffb99371
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-10 14:55:24 +0600

    Add mapping profile

---
+++ ProjectManagment.Application/Extensions/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ProjectManagment.Application.Dtos;
+using ProjectManagment.Domain.Entities;
+
+namespace ProjectManagment.Application.Extensions;
+
+public class MappingProfile: Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<UpdateProjectDto, Project>();
+        CreateMap<CreateProjectDto, Project>();
+    }
+}
