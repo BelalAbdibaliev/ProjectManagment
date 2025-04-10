9a4d546768a9b7f22480baabe4bf1a2f9a3a12a8
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-10 14:56:07 +0600

    Configure Application layer DI

---
+++ ProjectManagment.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProjectManagment.Application.Interfaces.Services;
+using ProjectManagment.Application.Services;
+
+namespace ProjectManagment.Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddScoped<IProjectService, ProjectService>();
+        services.AddAutoMapper(typeof(MappingProfile));
+        
+        return services;
+    }
+}
