abcacb3e14aae97c2345acdbacece38215c301c1
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-10 14:52:29 +0600

    Add dto

---
+++ ProjectManagment.Application/Dtos/CreateProjectDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagment.Application.Dtos;
+
+public class CreateProjectDto
+{
+    public string Name { get; set; } = null!;
+    public int Priority { get; set; }
+    public int ClientId { get; set; }
+    public int SupplierId { get; set; }
+    public int ProjectLeadId { get; set; }
+    public int EmployeeId { get; set; }
+}
---
+++ ProjectManagment.Application/Dtos/UpdateProjectDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjectManagment.Application.Dtos;
+
+public class UpdateProjectDto
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int Priority { get; set; } = 0;
+    public int ClientId { get; set; } = 0;
+    public int SupplierId { get; set; } = 0;
+    public int EmployeeId { get; set; } = 0;
+}    
