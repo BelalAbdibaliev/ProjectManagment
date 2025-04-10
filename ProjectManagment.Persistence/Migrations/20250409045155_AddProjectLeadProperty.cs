1d66c4a1f00c21a7880d1b80eef94a43b345232f
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-10 14:56:58 +0600

    Add migrations

---
+++ ProjectManagment.Persistence/Migrations/20250409045155_AddProjectLeadProperty.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagment.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProjectLeadProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectLeadId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Projects",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_ProjectLeadId",
+                table: "Projects",
+                column: "ProjectLeadId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Employees_ProjectLeadId",
+                table: "Projects",
+                column: "ProjectLeadId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Employees_ProjectLeadId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_ProjectLeadId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectLeadId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Projects");
+        }
+    }
+}
---
+++ ProjectManagment.Persistence/Migrations/20250410084940_SeedInitialEmployee.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectManagment.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialEmployee : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName", "MiddleName", "Email" },
+                values: new object[] { 1, "Belal", "Abdibaliev", "Mederbekovish", "flacko@example.com" }
+            );
+            migrationBuilder.InsertData(
+                table: "Employees",
+                columns: new[] { "Id", "FirstName", "LastName", "MiddleName", "Email" },
+                values: new object[] { 2, "Mirbek", "Polov", "Marat", "mirbek@example.com" }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1
+            );
+            migrationBuilder.DeleteData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2
+            );
+        }
+    }
+}
