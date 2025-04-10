915b6b225fd830467ec4f788dfeae672f260d3aa
Belal Abdibaliev <belalabdibaliev55@gmail.com>
2025-04-10 14:55:41 +0600

    Add project service

---
+++ ProjectManagment.Application/Interfaces/Services/IProjectService.cs
@@ -0,0 +1,13 @@
+﻿using ProjectManagment.Application.Dtos;
+using ProjectManagment.Domain.Entities;
+
+namespace ProjectManagment.Application.Interfaces.Services;
+
+public interface IProjectService
+{
+    Task<List<Project>> GetAllProjectsAsync(int pageNumber, int pageSize);
+    Task<Project> GetProjectByIdAsync(int id);
+    Task CreateProjectAsync(CreateProjectDto project);
+    Task UpdateProjectAsync(UpdateProjectDto project);
+    Task DeleteProjectAsync(int projectId);
+}
---
+++ ProjectManagment.Application/Services/ProjectService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using ProjectManagment.Application.Dtos;
+using ProjectManagment.Application.Interfaces.Repositories;
+using ProjectManagment.Application.Interfaces.Services;
+using ProjectManagment.Domain.Entities;
+
+namespace ProjectManagment.Application.Services;
+
+public class ProjectService: IProjectService
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
+
+    public ProjectService(IUnitOfWork unitOfWork, IMapper mapper)
+    {
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
+    }
+
+
+    public async Task<List<Project>> GetAllProjectsAsync(int pageNumber, int pageSize)
+    {
+        var projects = await _unitOfWork.Projects.GetAll(pageNumber, pageSize);
+        return projects.ToList();
+    }
+
+    public async Task<Project> GetProjectByIdAsync(int id)
+    {
+        var project = await _unitOfWork.Projects.GetById(id);
+        return project;
+    }
+
+    public async Task CreateProjectAsync(CreateProjectDto projectDto)
+    {
+        var project = _mapper.Map<Project>(projectDto);
+        await _unitOfWork.Projects.Add(project);
+        await _unitOfWork.SaveAsync();
+    }
+
+    public async Task UpdateProjectAsync(UpdateProjectDto projectDto)
+    {
+        var project = _mapper.Map<Project>(projectDto);
+        await _unitOfWork.Projects.Update(project);
+        await _unitOfWork.SaveAsync();
+    }
+
+    public async Task DeleteProjectAsync(int projectId)
+    {
+        await _unitOfWork.Projects.Delete(projectId);
+        await _unitOfWork.SaveAsync();
+    }
+}
